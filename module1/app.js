b467b435cef7507d4a2e43f8c82d880bc656e8c0
Nick Kostikis <nikkostikis@gmail.com>
2016-09-20 01:56:15 +0300

    Refactoring

--- module1/app.js
+++ module1/app.js
@@ -8,17 +8,8 @@
     $scope.lunchItemsStr = "";
     $scope.msg = "";
     $scope.checkItems = function(){
-      var lunchItemsArray = $scope.lunchItemsStr.split(",");
-      console.log(lunchItemsArray);
-      lunchItemsArray = lunchItemsArray.map(function(item){
-        return item.trim();
-      });
-      console.log(lunchItemsArray);
-      var filteredLunchItemsArray = lunchItemsArray.filter(function(item){
-        return (item !== (""));
-      });
-      console.log(filteredLunchItemsArray);
       $scope.colorClass = "green";
+      var filteredLunchItemsArray = cleanArray($scope.lunchItemsStr);
       if (filteredLunchItemsArray.length == 0) {
         $scope.msg = "Please enter data first";
         $scope.colorClass = "red";
@@ -30,7 +21,19 @@
       } else {
         $scope.msg = filteredLunchItemsArray.length + " items. Too much!";
       }
-    };
+    }; //checkItems
+  } //LunchCheckController
 
-  }
+  var cleanArray = function(input){
+    var lunchItemsArray = input.split(",");
+    lunchItemsArray = lunchItemsArray.map(function(item){
+      return item.trim();
+    });
+    var filteredLunchItemsArray = lunchItemsArray.filter(function(item){
+      // return (item !== (""));
+      return (item);
+    });
+    return filteredLunchItemsArray;
+  }; //cleanArray
+
 })();
